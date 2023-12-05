739e520a3bd1de755099acfa5f0cc718ad5ff5d8
Юрий Запатоцкий <srtxtex@gmail.com>
2023-12-05 03:10:00 +0300

    HW3: Добавление документации в код

--- hw3/predict_with_api.py
+++ hw3/predict_with_api.py
@@ -10,8 +10,10 @@ classifire = pipeline("sentiment-analysis")
 
 @app.get("/")
 def root():
+    """Тестовое сообщение для проверки работоспособности"""
     return {"message": "Wellcome to app!"}
 
 @app.post("/predict")
 def predict(item: Item):
+    """Предсказание настроения текста"""
     return classifire(item.text)[0]
